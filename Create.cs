d0cefabbda2d7cc33db56c09b5032dd732f59696
NaliniPogula <43030417+NaliniPogula@users.noreply.github.com>
2022-05-06 11:59:43 +0530

    Add files via upload

---
+++ Create.cs
@@ -0,0 +1,48 @@
+﻿//using HOL_4.Models;
+
+//namespace HOL_4.Migrations
+//{
+//    using System;
+//    using System.Data.Entity.Migrations;
+
+//    public partial class Create : DbMigration
+//    {
+//        public override void Up()
+//        {
+//            AlterColumn("dbo.Accounts", "Name", c => c.String(nullable: false, maxLength: 255));
+//        }
+
+//        public override void Down()
+//        {
+//            AlterColumn("dbo.Accounts", "Name", c => c.String());
+//        }
+//    }
+//}
+
+namespace HOL_4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+            "dbo.Accounts",
+            c => new
+            {
+                AccountNumber = c.Int(nullable: false),
+                Name = c.String(),
+                CurrentBalance = c.Double(nullable: false),
+            })
+            .PrimaryKey(t => t.AccountNumber);
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.Accounts");
+        }
+    }
+}
---
+++ MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.Entity;
+
+namespace HOL_4.Models
+{
+    public class MyDbContext : System.Data.Entity.DbContext
+    {
+        public MyDbContext()
+            : base("name=conn")
+        {
+
+        }
+        public System.Data.Entity.DbSet<Account> Accounts { get; set; }
+    }
+}
